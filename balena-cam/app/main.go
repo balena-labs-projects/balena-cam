282c4752c77e88811c117464558d83e563fd7375
Marios Balamatsias <mbalamatsias@gmail.com>
2018-12-10 22:14:03 +0200

    main: Add pions/webrtc lib, send sample vp8 video to client
    Signed-off-by: Marios Balamatsias <marios@balena.io>

--- balena-cam/app/main.go
+++ balena-cam/app/main.go
@@ -4,26 +4,57 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
+	"io/ioutil"
+	"github.com/pions/webrtc"
+	"github.com/pions/webrtc/examples/util"
+	"github.com/pions/webrtc/examples/util/gstreamer-src"
+	"github.com/pions/webrtc/pkg/ice"
 )
 
 func handleOffer(w http.ResponseWriter, r *http.Request){
-	var dat map[string]interface{}
-	if err := json.NewDecoder(r.Body).Decode(&dat); err != nil {
-		panic(err)
+	// extract offerer's sdp from json data
+	var offer webrtc.RTCSessionDescription
+	body, err := ioutil.ReadAll(r.Body)
+	util.Check(err)
+	err = json.Unmarshal(body, &offer)
+	util.Check(err)
+	// WebRTC
+	webrtc.RegisterDefaultCodecs()
+	config := webrtc.RTCConfiguration {
+		IceServers: []webrtc.RTCIceServer {
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
 	}
-	//INFO: dat["sdp"] has viewer's offer sdp
-	/*
-	if dat["type"] == "offer" {
-		fmt.Println(dat["sdp"])
-	}
-	*/
-	answerJson := []byte(`{"hello":"world"}`)
+	peerConnection, err := webrtc.New(config)
+	util.Check(err)
+	peerConnection.OnICEConnectionStateChange(func(connectionState ice.ConnectionState) {
+		fmt.Printf("Connection State has changed %s \n", connectionState.String())
+	})
+	// Create Video Track
+	vp8Track, err := peerConnection.NewRTCSampleTrack(webrtc.DefaultPayloadTypeVP8, "video", "pion2")
+	util.Check(err)
+	_, err = peerConnection.AddTrack(vp8Track)
+	util.Check(err)
+	// Set offer
+	err = peerConnection.SetRemoteDescription(offer)
+	util.Check(err)
+	answer, err := peerConnection.CreateAnswer(nil)
+	util.Check(err)
+	// Make and Send answer back to the client
+	answerJson, err := json.Marshal(answer)
+	util.Check(err)
+	gst.CreatePipeline(webrtc.VP8, vp8Track.Samples).Start()
+
+	// Response
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(answerJson)
 }
 
 func main(){
+	// Web Server
 	dir := http.FileServer(http.Dir("client"))
 	http.Handle("/", dir)
 	http.HandleFunc("/offer", handleOffer)
