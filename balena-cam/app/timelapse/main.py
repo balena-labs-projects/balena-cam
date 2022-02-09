15efc654400f98cd0bccdc57debe8de62774e716
Richard Morrison <richard@rmorrison.net>
2022-02-09 22:31:42 +0000

    Dummy uploads outside daylight hours

--- balena-cam/app/timelapse/main.py
+++ balena-cam/app/timelapse/main.py
@@ -30,8 +30,8 @@ def take_a_photo(cmd=PHOTO_JPG_CMD):
 
 
 def upload_to_s3(data, captured_at, s3_bucket=S3_BUCKET):
-    logging.info("Uploading to s3...")
     s3_key = captured_at.isoformat().replace("+00:00", "Z") + ".jpg"
+    logging.info("Uploading %s to s3...", s3_key)
     s3 = boto3.client("s3")
     s3.upload_fileobj(io.BytesIO(data), s3_bucket, s3_key)
 
@@ -44,3 +44,9 @@ def tick():
     if now.time() >= earliest and now.time() <= latest:
         result, when = take_a_photo()
         upload_to_s3(data=result.stdout, captured_at=when)
+    else:
+        # a dummy upload, so that monitoring still stays green
+        upload_to_s3(
+            data=b"abc",
+            captured_at=dt.datetime(2000, 1, 1, 0, 0, tzinfo=dt.timezone.utc),
+        )
